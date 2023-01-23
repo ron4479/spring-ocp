468efe69e7e17e671e39aa2e9e8d3e235bf4e8fb
Ronan Nival <ronan@MacBook-Pro-de-Ronan.local>
2023-01-23 11:17:18 +0100

    nouveau projet

---
+++ src/main/java/com/example/springocp/SpringOcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.springocp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOcpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springocp/SpringOcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springocp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
